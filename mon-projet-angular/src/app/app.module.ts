c24ade8477ee03cf76222f539b3cd18fec77ffdb
sfobo <renandekersabiec@gmail.com>
2019-08-06 08:50:58 +0200

    Cour Gérez la navigation avec le Routing (Partie Router et Router link

--- mon-projet-angular/src/app/app.module.ts
+++ mon-projet-angular/src/app/app.module.ts
@@ -8,15 +8,28 @@ import { AppareilComponent } from './appareil/appareil.component';
 import { FormsModule } from '@angular/forms';
 
 import { AppareilService } from "./services/appareil.service";
+import { AuthComponent } from './auth/auth.component';
+import { AppareilViewComponent } from './appareil-view/appareil-view.component';
+import { RouterModule, Routes } from "@angular/router";
+
+const appRoutes: Routes = [
+  { path:'appareil', component: AppareilViewComponent },
+  { path:'auth', component: AuthComponent },
+  { path:'', component: AppareilViewComponent }
+];
+
 @NgModule({
   declarations: [
     AppComponent,
     MonPremierComponent,
-    AppareilComponent
+    AppareilComponent,
+    AuthComponent,
+    AppareilViewComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     AppareilService
